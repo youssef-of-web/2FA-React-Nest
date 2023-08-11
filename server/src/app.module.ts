d3df815f5c778c079ecc978a388d8bf4553c1473
mansouri youssef <mansouriyoussef1991@gmail.com>
2023-08-11 21:27:24 +0100

    Update app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { PrismaService } from './prisma.service';
-import { UsersModule } from './users/users.module';
+
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), AuthModule],
   controllers: [],
   providers: [],
 })
